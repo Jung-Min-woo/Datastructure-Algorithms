d83868a0e2a9df622474e4bcfa04e3efc806d97e
JungMinwoo <jungmw95@gmail.com>
2020-05-13 00:13:40 +0900

    linkedlist

---
+++ datastructure/linkedlist.c
@@ -0,0 +1,66 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int LData;
+
+typedef struct _node {
+	int data;
+	struct _node *next;
+}Node;
+typedef struct _linkedlist {
+	Node *head;
+	Node *cur;
+	Node *before;
+	int numofData;
+	int (*comp)(LData d1, LData d2);
+}List;
+
+void ListInit(List *plist);
+void LInsert(List *plist, LData data);
+void SInsert(List *plist, LData data);
+int LFirst(List *plist, LData *pdata);
+int LNext(List *plist, LData *pdata);
+LData LRemove(List *plist);
+int LCount(List *plist);
+void SetSortRule(List *plist, int(*comp)(LData d1, LData d2));
+
+int main(void) {
+	List list;
+	int data;
+	ListInit(&list);
+
+	LInsert(&list, 11); LInsert(&list, 11);
+	LInsert(&list, 22); LInsert(&list, 22);
+	LInsert(&list, 33);
+
+	printf("현재 데이터의 수 : %d\n", LCount(&list));
+	if (LFirst(&list, &data)) {
+		printf("%d ", data);
+
+		while (LNext(&list, &data))
+			printf("%d ", data);
+	}
+	printf("\n\n");
+
+	if (LFirst(&list, &data)) {
+		if (data == 22)
+			LRemove(&list);
+		while (LNext(&list, &data)) {
+			if (data == 22)
+				LRemove(&list);
+		}
+	}
+	printf("현재 데이터의 수 : %d\n", LCount(&list));
+	if (LFirst(&list, &data)) {
+		printf("%d ", data);
+
+		while (LNext(&list, &data))
+			printf("%d ", data);
+	}
+	printf("\n\n");
+	return 0;
+}
