d1f1dbc686a22aa65be07a654d2a62e7c93d0c73
JungMinwoo <jungmw95@gmail.com>
2020-05-24 17:07:27 +0900

    CircularList completed

--- datastructure/list.cpp
+++ datastructure/list.cpp
@@ -7,11 +7,8 @@
 #define TRUE 1
 #define FALSE 0
 
-#define CIRCULAR_LIST
+typedef Point* LData;
 
-typedef int LData;
-
-#ifdef LINKED_LIST_1
 typedef struct _node {
 	LData data;
 	struct _node *next;
@@ -23,6 +20,7 @@ typedef struct _linkedlist {
 	int numofData;
 	int (*comp)(LData d1, LData d2);
 }List;
+
 void ListInit(List *plist) {
 	Node *DummyNode = (Node*)malloc(sizeof(Node));
 
@@ -38,7 +36,7 @@ void FInsert(List *plist, LData data) {
 	Node *newNode = (Node*)malloc(sizeof(Node));
 	newNode->data = data;
 	newNode->next = plist->head->next;
-	plist->head->next = newNode;  
+	plist->head->next = newNode;
 	plist->numofData++;
 }
 void SInsert(List *plist, LData data) {
@@ -97,68 +95,4 @@ void SetSortRule(List *plist, int(*comp)(LData d1, LData d2)) {
 int bigger(int d1, int d2) {
 	return d1 < d2 ? 0 : 1;
 }
-
-#endif
-
-#ifdef CIRCULAR_LIST
-
-typedef struct _node {
-	LData data;
-	struct _node *next;
-}Node;
-typedef struct _linkedlist {
-	Node *tail;
-	Node *cur;
-	Node *before;
-	int numofData;
-}List;
-void ListInit(List *plist) {
-	plist->numofData = 0;
-	plist->tail = NULL;
-	plist->before = NULL;
-	plist->cur = NULL;
-}
-void LInsert(List *plist, LData data) {
-	Node *newNode = (Node*)malloc(sizeof(Node));
-	newNode->data = data;
-	if (plist->tail == NULL) {
-		plist->tail = newNode;
-		newNode->next = newNode;
-	}
-	else {
-		newNode->next = plist->tail->next;
-		plist->tail->next = newNode;
-	}
-	plist->numofData++;
-}
-int LFirst(List *plist, LData *pdata) {
-	plist->cur = plist->tail->next;
-	plist->before = plist->tail;
-	if (plist->cur == NULL)
-		return FALSE;
-	*pdata = plist->cur->data;
-	return TRUE;
-}
-int LNext(List *plist, LData *pdata) {
-	plist->before = plist->cur;
-	plist->cur = plist->cur->next;
-	if (plist->cur == NULL)
-		return FALSE;
-	*pdata = plist->cur->data;
-	return TRUE;
-}
-LData LRemove(List *plist) {
-	LData out = plist->cur->data;
-	plist->numofData--;
-	if (plist->before->next != NULL)
-		plist->before->next = plist->cur->next;
-	free(plist->cur);
-	plist->cur = plist->before;
-	return out;
-}
-int LCount(List *plist) {
-	return plist->numofData;
-}
-#endif
-
 #endif
