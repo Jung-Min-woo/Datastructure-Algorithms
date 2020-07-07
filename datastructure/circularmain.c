db472c0d899aa50ac42bd595aadc7dc31d1cdc19
JungMinwoo <jungmw95@gmail.com>
2020-07-07 20:38:09 +0900

    ArrayBased_Circular_Queue

---
+++ datastructure/circularmain.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "CircularQueue.h"
+
+int main(void) {
+	Queue q;
+	QueueInit(&q);
+	int i = 0;
+	for(i=1;i<6;i++) Enqueue(&q, i);
+	while (!QIsEmpty(&q)) printf("%d ", Dequeue(&q));
+	return 0;
+}
