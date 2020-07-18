4892916c8adc89af054e79242d1f44014546c6d8
JungMinwoo <jungmw95@gmail.com>
2020-07-18 12:51:09 +0900

    +InsertSort

--- datastructure/SimpleSort.c
+++ datastructure/SimpleSort.c
@@ -1,17 +1,35 @@
 #include <stdio.h>
 void BubbleSort(int arr[], int n);
 void SelSort(int arr[], int n);
-
+void InserSort(int arr[], int n);
 int main(void) {
-	int arr[4] = { 3,2,4,1 };
+	int arr[5] = { 5,3,2,4,1 };
 	int i;
-	SelSort(arr, sizeof(arr) / sizeof(int));
-	for (int i = 0; i < 4; i++)
+	InserSort(arr, sizeof(arr) / sizeof(int));
+	for (int i = 0; i < 5; i++)
 		printf("%d ", arr[i]);
 	printf("\n");
 	return 0;
 }
-
+void InserSort(int arr[], int n) {
+	int i, j, idx, temp;
+	for (i = 1; i < n; i++) {
+		temp = arr[i];
+		idx = i;
+		for (j = 0; j < i; j++) {
+			if (arr[j] > arr[i]) {
+				idx = j;
+				break;
+			}
+		}
+		printf("idx : %d, temp: %d\n", idx, temp);
+		for (j = i; j >=idx; j--) {
+			arr[j] = arr[j-1];
+		}
+		arr[idx] = temp;
+		printf("arr: %d %d %d %d\n", arr[0], arr[1], arr[2], arr[3]);
+	}
+}
 void SelSort(int arr[], int n) {
 	int temp= arr[0], target=0, idx = 0;
 	int i;
