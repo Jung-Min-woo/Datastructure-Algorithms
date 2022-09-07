936a2a365cf386ee734c733fc2776ca7a646a0c0
root <root@insuNote28.localdomain>
2022-09-07 13:25:15 +0900

    no comment

--- datastructure/RecursiveBinarySearch.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int BSearchRecur(int arr[],int first, int last, int target) {
-	int mid = (first + last) / 2;
-	if (first > last) return -1;
-	if (arr[mid] == target) return target;
-	else if (arr[mid] < target) BSearchRecur(arr, mid+1, last, target);
-	else BSearchRecur(arr, first, mid-1, target);
-}
