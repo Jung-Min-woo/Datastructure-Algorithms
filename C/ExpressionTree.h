afaa0b5e7707f593d818137d5c6a7f437f886e77
jungminwoo <jungmw95@gmail.com>
2022-09-07 13:46:33 +0900

    no comment

--- C/ExpressionTree.h
+++
@@ -1,9 +0,0 @@
-#ifndef __EXPRESSIONTREE_H__
-#define __EXPRESSIONTREE_H__
-#include "BinaryTree.h"
-BTreeNode * MakeExpTree(char exp[]);
-int EvaluateExpTree(BTreeNode *bt);
-void ShowPrefixTypeExp(BTreeNode *bt);
-void ShowPostfixTypeExp(BTreeNode *bt);
-void ShowInfixTypeExp(BTreeNode *bt);
-#endif
